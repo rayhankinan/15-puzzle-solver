bd2d693b83113f383f6e9accd2d60c158cd30201
rayhankinan <rayhankinan@gmail.com>
2022-03-29 03:03:24 +0700

    Minor changes

--- src/puzzle.py
+++ src/puzzle.py
@@ -1,4 +1,6 @@
 class PositionMatrix:
+    legalElement = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "-"}
+
     def __init__(self, data):
         self.matrix = []
 
@@ -9,18 +11,27 @@ class PositionMatrix:
 
         else:
             for row in listOfRow:
-                if len(row) != 4:
+                listOfElement = row.split(" ")
+
+                if len(listOfElement) != 4:
                     raise Exception(f"Jumlah kolom pada file txt harus berjumlah 4! Terdapat kolom pada file dengan jumlah {len(row)}.")
 
                 else:
                     temp = []
-                    for element in row.split(" "):
-                        temp.append(int(element) if element.isnumeric() else None)
+                    for element in listOfElement:
+                        if element not in PositionMatrix.legalElement:
+                            raise Exception(f"Terdapat elemen ilegal pada file txt! Elemen ilegal tersebut adalah \"{element}\".")
+
+                        else:
+                            temp.append(int(element) if element.isnumeric() else None)
+
                     self.matrix.append(temp)
 
 if __name__ == "__main__":
-    data = "1 2 3 4\n5 6 7 8\n9 10 11 12\n13 14 15 -\n"
+    try:
+        data = "1 2 3 4\n5 6 7 8\n9 10 11 12\n13 14 15 -"
+        PM = PositionMatrix(data)
+        print(PM.matrix)
 
-    PM = PositionMatrix(data)
-
-    print(PM.matrix)
+    except Exception as e:
+        print(e)
